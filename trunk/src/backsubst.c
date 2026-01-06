240049e18baf7e58e8783d14bd621d8f2cd73dec
Aftertower8 <powierzaaleksander9@gmail.com>
2026-01-06 14:04:12 +0100

    naprawda backsubst.c 2

--- trunk/src/backsubst.c
+++ trunk/src/backsubst.c
@@ -12,18 +12,15 @@ int  backsubst(Matrix *x, Matrix *mat, Matrix *b) {
 	//mat to macierz z niewiadomymi, b to macierz wynikami rownan, x to macierz do ktorej mam wstawic wartosci niewiadomych
 	if(mat->r!=b->r || mat->c!=x->r || mat->c!=mat->r)
 		return 2;
-	int kolumna_akt_x = mat->c-1;
 	for(int i=mat->r-1;i>=0;i--){
 		double wartosc_akt_x=b->data[i][0];
-		for(int j=kolumna_akt_x+1; j<mat->c; j++){
+		for(int j=i+1; j<mat->r; j++){
 			wartosc_akt_x-=(mat->data[i][j])*(x->data[j][0]);	//przechodze po danym wierszu i dodaje do wartosc_akt_x,
 			//od razu podstawiam pod wyliczone wczesniej x ich wartosci (przechodze po kolumnach macierzy x - odpowiadaja im wiersze macierzy m)
 		}
-		if(mat->data[i][kolumna_akt_x]==0)
+		if(mat->data[i][i]==0.0)
 			return 1;	//blad - dzielenie przez 0
-		wartosc_akt_x/=mat->data[i][kolumna_akt_x];
-		x->data[i][0]=wartosc_akt_x;
-		kolumna_akt_x--;
+		x->data[i][0]=wartosc_akt_x/mat->data[i][i];
 	}
 	return 0;
 }
